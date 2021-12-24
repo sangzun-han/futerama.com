13b52d5f0a1c1357b91d4b7b061c9f8797f9ea90
sangzun-han <hsjun1996@naver.com>
2021-12-25 03:30:00 +0900

    character responsive

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -10,19 +10,36 @@ export const CharacterCard = ({ characterData }: CharacterCardProps) => {
   return (
     <Card>
       <Profile src={images.main} alt="character" />
-      <h2>
-        {name.first} {name.middle} {name.last}
-      </h2>
+      <H2>
+        <p>
+          {name.first} {name.middle} {name.last}
+        </p>
+      </H2>
     </Card>
   );
 };
 
 const Card = styled.div`
-  max-width: 360px;
+  max-width: 280px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  &:hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+  }
+
+  @media screen and (max-width: 480px) {
+    width: 100%;
+  }
 `;
 
 const Profile = styled.img`
   width: 100%;
-  aspect-ratio: 9 /16;
+  aspect-ratio: 4 / 3;
   object-fit: contain;
+`;
+
+const H2 = styled.h2`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
 `;
--- src/components/CharacterCardContainer.tsx
+++ src/components/CharacterCardContainer.tsx
@@ -29,8 +29,19 @@ export const CharacterCardContainer = () => {
 const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
+  gap: 25px;
+  padding: 2rem;
+
+  @media screen and (max-width: 1024px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
 
   @media screen and (max-width: 768px) {
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media screen and (max-width: 480px) {
+    grid-template-columns: repeat(1, 1fr);
+    place-items: center;
   }
 `;

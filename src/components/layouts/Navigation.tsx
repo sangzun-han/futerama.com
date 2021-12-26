5159aef0b4d011a6087ff91fc2c5b003b818e7a9
sangzun-han <hsjun1996@naver.com>
2021-12-26 09:55:23 +0900

    route

--- src/components/layouts/Navigation.tsx
+++ src/components/layouts/Navigation.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import Link from "next/link";
 import { ROUTES } from "../../constants";
 
@@ -9,14 +10,77 @@ interface ROUTE {
 
 export const Navigation = () => {
   return (
-    <div>
+    <Header>
+      <Logo>
+        <Img src="logo.png" alt="logo" />
+      </Logo>
+      <Burger>
+        <Line></Line>
+        <Line></Line>
+        <Line></Line>
+      </Burger>
       {ROUTES.map((routeObject: ROUTE) => {
         return (
-          <Link key={`navbar-${routeObject.ID}`} href={routeObject.PATH}>
-            <a>{routeObject.LABEL}</a>
-          </Link>
+          <div>
+            <Link key={`navbar-${routeObject.ID}`} href={routeObject.PATH}>
+              <Item>{routeObject.LABEL}</Item>
+            </Link>
+          </div>
         );
       })}
-    </div>
+    </Header>
   );
 };
+
+const Item = styled.li`
+  list-style: none;
+  font-size: 20px;
+
+  @media screen and (max-width: 1280px) {
+    display: none;
+  }
+`;
+
+const Header = styled.header`
+  width: 1280px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media screen and (max-width: 1280px) {
+    width: 90%;
+  }
+`;
+
+const Logo = styled.div`
+  display: flex;
+  padding: 1.5rem;
+`;
+
+const Img = styled.img`
+  max-width: 250px;
+  background-color: transparent;
+
+  @media screen and (max-width: 1280px) {
+    width: 70%;
+  }
+`;
+
+const Burger = styled.div`
+  display: none;
+
+  @media screen and (max-width: 1280px) {
+    display: block;
+    cursor: pointer;
+    position: absolute;
+    right: 3%;
+  }
+`;
+
+const Line = styled.div`
+  width: 35px;
+  height: 5px;
+  background-color: #333;
+  margin: 5px 0px;
+`;
